224e5c8f679b1d0803b88fc4019002f140f8c290
weisong0908 <hi@tengweisong.com>
2020-07-26 18:33:40 +0800

    makes title animation plays only once and allows for replay

--- base.js
+++ base.js
@@ -2,14 +2,44 @@ $(document).ready(() => {
   const heroTitle1 = "Hi, I'm Wei Song -";
   const heroTitle2 = "I'm a web developer based in Singapore.";
 
-  animateText(heroTitle1, ".hero-title-1", 2500);
+  const playTitleAnimation = sessionStorage.getItem("play-title-animation");
+
+  if (playTitleAnimation == null || playTitleAnimation == true) {
+    clearTitles();
+    animateText(heroTitle1, ".hero-title-1", 2500);
+  } else {
+    $(".hero-title-1").html(heroTitle1);
+    $(".hero-title-2")
+      .html(heroTitle2)
+      .append("<span class='caret' aria-hidden='true'></span>");
+  }
 
   $(".hero-container").on("hero-title-1:done", (event) => {
     $(".caret").remove();
     $(".hero-title-2").append("<span class='caret' aria-hidden='true'></span>");
     animateText(heroTitle2, ".hero-title-2", 1000);
   });
+
+  $(".hero-container").on("hero-title-2:done", (event) => {
+    sessionStorage.setItem("play-title-animation", false);
+    $(".play-animation-btn").show();
+  });
+
+  $(".play-animation-btn").on("click", (event) => {
+    event.preventDefault();
+    sessionStorage.setItem("play-title-animation", true);
+    clearTitles();
+    animateText(heroTitle1, ".hero-title-1", 2500);
+  });
 });
+
+function clearTitles() {
+  $(".hero-title-1")
+    .empty()
+    .append("<span class='caret' aria-hidden='true'></span>");
+  $(".hero-title-2").empty();
+  $(".play-animation-btn").hide();
+}
 
 function animateText(title, selector, delay) {
   setTimeout(() => {
