2289ddaa3afa97403e4c52fdd4e7f5137bc4f862
weisong0908 <hi@tengweisong.com>
2020-07-26 15:19:27 +0800

    added animation for hero-title-2

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 $(document).ready(() => {
-  const heroTitle1 = "Hi, I'm Wei Song";
+  const heroTitle1 = "Hi, I'm Wei Song -";
+  const heroTitle2 = "I'm a web developer based in Singapore.";
 
-  setTimeout(() => {
-    animateText(heroTitle1, ".animate-text-1");
-  }, 3000);
+  animateText(heroTitle1, ".hero-title-1", 2500);
+
+  $(".hero-container").on("hero-title-1:done", (event) => {
+    $(".caret").remove();
+    $(".hero-title-2").append("<span class='caret' aria-hidden='true'></span>");
+    animateText(heroTitle2, ".hero-title-2", 1000);
+  });
 });
 
-function animateText(title, selector) {
-  typeWrite(title, selector, 0);
+function animateText(title, selector, delay) {
+  setTimeout(() => {
+    typeWrite(title, selector, 0);
+  }, delay);
 }
 
 function typeWrite(text, selector, i) {
   if (i < text.length) {
     $(selector)
-      .text(text.substring(0, i + 1))
+      .html(text.substring(0, i + 1))
       .append("<span class='caret' aria-hidden='true'></span>");
 
     setTimeout(function () {
       typeWrite(text, selector, i + 1);
-    }, 125);
+    }, 100);
+  } else {
+    setTimeout(() => {
+      $(".hero-container").trigger(`${selector.substring(1)}:done`);
+    }, 500);
   }
 }
