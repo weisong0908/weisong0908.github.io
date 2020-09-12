d76eb769070523f6589c41d1e77b11cb2787f75e
weisong0908 <hi@tengweisong.com>
2020-09-13 03:18:59 +0800

    updated home page

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
+  <div class="main-container">
     <header>
       <nav>
-        <g-link to="/"><img src="/favicon.png" alt="Home"/></g-link>
+        <g-link class="site-logo" to="/"
+          ><img src="/favicon.png" alt="Home"
+        /></g-link>
         <g-link to="/about/">About</g-link>
         <g-link to="/works/">Works</g-link>
         <a href="https://blog.tengweisong.com">Blog</a>
@@ -15,7 +17,7 @@
         <a href="https://www.linkedin.com/in/tengweisong/">LinkedIn</a>
         <a href="https://github.com/weisong0908">Github</a>
       </nav>
-      <div>
+      <div class="footer-copyright">
         <small>&copy; Teng Wei Song 2020</small>
       </div>
     </footer>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,18 @@
 <template>
   <default-layout>
-    <h1>Hi, I'm Wei Song -</h1>
-    <h1>I'm a web developer.</h1>
+    <div class="hero-container">
+      <h1 class="hero-title hero-title-1">Hi, I'm Wei Song</h1>
+      <h1 class="hero-title hero-title-2">
+        I'm a web developer based in Singapore.
+        <span class="caret"></span>
+      </h1>
+      <br />
+      <a class="play-animation-btn">
+        <small>
+          Play animation
+        </small>
+      </a>
+    </div>
   </default-layout>
 </template>
 
@@ -13,4 +24,42 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.hero-container {
+  padding: 2rem 1rem;
+  min-height: 35rem;
+  max-width: 50rem;
+  margin: 0 auto;
+}
+
+.hero-title {
+  font-size: 3.5rem;
+  font-weight: normal;
+}
+
+.hero-title:first-child {
+  font-weight: bold;
+}
+
+span.caret {
+  border-right: 0.5rem solid var(--accent-color-highlight);
+  animation: caret 1s steps(1) infinite;
+}
+
+@keyframes caret {
+  50% {
+    border-color: transparent;
+  }
+}
+
+a.play-animation-btn {
+  padding: 0rem;
+  color: var(--accent-color-neutral);
+}
+
+a.play-animation-btn:hover {
+  background-color: var(--accent-color-neutral);
+  color: var(--accent-color-light);
+  cursor: pointer;
+}
+</style>
