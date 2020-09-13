90652c9506fcd63e20354d864b452a6b1d4e97e0
Teng Wei Song <weisong.teng.work@gmail.com>
2020-09-13 20:12:35 +0800

    animate title for homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,26 +1,41 @@
 <template>
   <default-layout>
     <div class="hero-container">
-      <h1 class="hero-title hero-title-1">Hi, I'm Wei Song</h1>
+      <h1 class="hero-title hero-title-1">
+        {{ heroTitle1Text }}
+        <span class="caret" v-show="isInHeroTitle1"></span>
+      </h1>
       <h1 class="hero-title hero-title-2">
-        I'm a web developer based in Singapore.
-        <span class="caret"></span>
+        {{ heroTitle2Text }}
+        <span class="caret" v-show="isInHeroTitle2"></span>
       </h1>
       <br />
-      <a class="play-animation-btn">
-        <small>
-          Play animation
-        </small>
+      <a class="play-animation-btn" :v-show="isPlayButtonShown">
+        <small>Play animation</small>
       </a>
     </div>
   </default-layout>
 </template>
 
 <script>
+import animateTitle from "../assets/animate.js";
+
 export default {
   metaInfo: {
-    title: "Home"
-  }
+    title: "Home",
+  },
+  data() {
+    return {
+      heroTitle1Text: "",
+      heroTitle2Text: "",
+      isPlayButtonShown: true,
+      isInHeroTitle1: true,
+      isInHeroTitle2: false,
+    };
+  },
+  mounted() {
+    animateTitle();
+  },
 };
 </script>
 
