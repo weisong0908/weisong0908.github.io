7d4c0f2f51dc34abf360e43995b988ec8aff5af2
Teng Wei Song <weisong.teng.work@gmail.com>
2022-07-10 09:24:06 +0800

    make navbar responsive

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,20 +1,15 @@
 <template>
-  <div class="my-8">
-    <ul class="flex flex-row space-x-16 items-center justify-center">
-      <li>
-        <NuxtLink to="/" class="p-2 hover:bg-gray-200">Home</NuxtLink>
+  <nav class="my-8">
+    <ul
+      class="flex flex-row sm:space-x-16 space-x-4 items-center justify-center"
+    >
+      <li v-for="page in pages" :key="page.name">
+        <NuxtLink :to="page.link" class="p-2 hover:bg-gray-200">{{
+          page.title
+        }}</NuxtLink>
       </li>
-      <li>
-        <NuxtLink to="/about-me" class="p-2 hover:bg-gray-200">About Me</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/projects" class="p-2 hover:bg-gray-200">Projects</NuxtLink>
-      </li>
-        <li>
-          <NuxtLink to="/notes" class="p-2 hover:bg-gray-200">Notes</NuxtLink>
-        </li>
     </ul>
-  </div>
+  </nav>
 </template>
 
 <style scoped>
@@ -22,3 +17,30 @@
   @apply border-b-2 border-gray-200;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      pages: [
+        {
+          title: "Home",
+          link: "/",
+        },
+        {
+          title: "About Me",
+          link: "/about-me",
+        },
+        {
+          title: "Projects",
+          link: "/projects",
+        },
+        {
+          title: "Notes",
+          link: "/notes",
+        },
+      ],
+    };
+  },
+};
+</script>
