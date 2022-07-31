50962ce8ee6a8ddcb77205f8f86e67686681844e
Teng Wei Song <weisong.teng.work@gmail.com>
2022-07-31 21:18:50 +0800

    publish

---
+++ docs/_nuxt/static/1659273348/projects/ministry-of-finance/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659273348",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fministry-of-finance",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"01fb699a"}}}}(null));
