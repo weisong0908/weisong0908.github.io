eb671da1a004a673158b400b51f1003b0d3983fc
Teng Wei Song <weisong.teng.work@gmail.com>
2022-07-31 21:09:28 +0800

    publish

--- docs/_nuxt/static/1659199237/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout-me","\u002Fprojects","\u002Fnotes","\u002Fnotes\u002Fpackage-commit-differences","\u002Fprojects\u002Fam-buddy-app","\u002Fprojects\u002Fopen-innovation-network","\u002Fprojects\u002Fministry-of-finance","\u002Fprojects\u002Fsimple-app"]})
